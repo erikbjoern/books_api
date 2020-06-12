ae5439cd2c1fe704b3f3b9e5baea74efe5d780e9
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-12 09:52:54 +0200

    seeding authors associated with books

--- database/seeders/20200610084648-Book.js
+++ database/seeders/20200610084648-Book.js
@@ -1,22 +1,42 @@
 "use strict";
 
+const models = require('../../models')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert("Authors", [
+      {
+        name: "Juan",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "Bob",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ])
+
+    const authors = await models.Author.findAll()
+
     await queryInterface.bulkInsert("Books", [
       {
         title: "Really Nice Book",
         createdAt: new Date(),
         updatedAt: new Date(),
+        authorId: authors[0].id
       },
       {
         title: "Really Fine Book",
         createdAt: new Date(),
         updatedAt: new Date(),
+        authorId: authors[1].id
       },
     ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("Books", null, {})
+    await queryInterface.bulkDelete("Authors", null, {})
   },
 };
